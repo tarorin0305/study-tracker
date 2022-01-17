60de0927e58b71820155c217fd61b862f69cd2d4
tarorin0305 <dayutakatarorin@gmail.com>
2022-01-17 23:38:16 +0900

    RailsサーバーのAPIから取得した値を用いてチャートを描画するようにした

--- src/components/ChartList.tsx
+++ src/components/ChartList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -47,6 +48,26 @@ export const bar_options = {
     },
   },
 };
+
+export const h_bar_options = {
+  indexAxis: 'y' as const,
+  elements: {
+    bar: {
+      borderWidth: 2,
+    },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'right' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Horizontal Bar Chart',
+    },
+  },
+};
+
 export const grouped_bar_options = {
   plugins: {
     title: {
@@ -69,30 +90,34 @@ export const grouped_bar_options = {
   },
 };
 
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const labels = [...Array(30)].map((_, i) => {
+  let jstNow = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
+  jstNow.setDate(jstNow.getDate() - i)
+  return `${jstNow.getMonth() + 1}/${jstNow.getDate()}`
+}).reverse()
 const getRandomInt = (min: number, max: number): number => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min)
 }
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => getRandomInt(-100, 100)),
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => getRandomInt(-100, 100)),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
+// APIを叩いて以下のデータを取得
+// 勉強科目
+// 勉強したポモドーロ数
+// 指定したグラフの色
+// [
+//   {
+//     label: 'Distinction',
+//     data: [1, 2, 1, 1, 0, 5, ...],
+//     borderColor: 'rgb(255, 99, 132)',
+//     backgroundColor: 'rgba(255, 99, 132, 0.5)',
+//   },
+//   {
+//     label: 'Cambly',
+//     data: [1, 2, 1, 1, 0, 5, ...],
+//     borderColor: 'rgb(53, 162, 235)',
+//     backgroundColor: 'rgba(53, 162, 235, 0.5)',
+//   }
+// ]
 
 export const grouped_bar_data = {
   labels,
@@ -119,12 +144,25 @@ export const grouped_bar_data = {
 };
 
 const ChartList = () => {
+  const [studyRecords, setStudyRecords] = useState([])
+  useEffect(()=> {
+    const getStudyRecords = async () => {
+      const _response = await fetch(`http://localhost:8000/api/study_records`, {mode: 'cors'})
+        .then(res => res.json())
+        .then(setStudyRecords)
+    };
+    getStudyRecords()
+  }, [])
+  const data = {
+    labels,
+    datasets: studyRecords,
+  };
   return (
     <div className="chart-group w-3/4">
       <h2 className="text-3xl font-bold">チャート</h2>
-      <ul className="max-w-[90%] grid grid-cols-2 gap-x-4 gap-y-4">
+      <ul className="max-w-[100%] grid grid-cols-2 gap-x-4 gap-y-4">
         <li className="col-start-1 col-end-3"><Line options={line_options} data={data} /></li>
-        <li className=""><Bar options={bar_options} data={data} /></li>
+        <li className=""><Bar options={h_bar_options} data={data} /></li>
         <li className=""><Bar options={grouped_bar_options} data={grouped_bar_data} /></li>
       </ul>
     </div>
